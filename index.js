87c42267b029def8dd9297908a991c48eb2127c8
Shajeer Majeed <shajeer.majeed@urolime.com>
2022-03-05 21:44:38 +0530

    procfile addedeee

--- index.js
+++ index.js
@@ -27,7 +27,7 @@ app.get('/', (req, res) => {
     .end();
 });
 app.use("/auth", authRoute);
-
-app.listen("5000", () => {
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
   console.log("Server is running!");
 });
